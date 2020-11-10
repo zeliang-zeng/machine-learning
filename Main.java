149c148f1dc99b7d337b2d2dad62da0d15b8136c
zeliang-zeng <2496508879@qq.com>
2020-11-10 14:42:57 +0800

    第二次修改

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("ly");
-		System.out.print("zzl1");
-
+		System.out.print("zzl2");
+		
 	}
 	
 }
